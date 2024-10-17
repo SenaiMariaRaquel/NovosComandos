0038ecdce203934bed6af71acd850a6e27389f54
GustavodeCastrov <0001074799@senaimgaluno.com.br>
2024-10-17 17:14:54 -0300

    Adicionando Gustavo

--- ConhecendoGiteGirhub/src/CloneePull.java
+++ ConhecendoGiteGirhub/src/CloneePull.java
@@ -5,7 +5,7 @@ public class CloneePull {
 		System.out.println("Conhecendo os comandos git glone e git pull");
 		System.out.println("1 - Andressa S2");
 		System.out.println("2 - Felipe");
-		System.out.println("3 - ");
+		System.out.println("3 - Gustavo");
 		System.out.println("4 - ");
 		System.out.println("5 - ");
 		System.out.println("6 - ");
